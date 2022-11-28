984b265ed021f9e081319b2b8163ebb627ca9243
aliklay <117722678+aliklay@users.noreply.github.com>
2022-11-28 14:49:04 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace rebarApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMesh = new System.Windows.Forms.Button();
+            this.btnRebars = new System.Windows.Forms.Button();
+            this.cc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Radius = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMesh
+            // 
+            this.btnMesh.Location = new System.Drawing.Point(411, 415);
+            this.btnMesh.Name = "btnMesh";
+            this.btnMesh.Size = new System.Drawing.Size(75, 23);
+            this.btnMesh.TabIndex = 0;
+            this.btnMesh.Text = "Insert Mesh";
+            this.btnMesh.UseVisualStyleBackColor = true;
+            this.btnMesh.Click += new System.EventHandler(this.btnMesh_Click);
+            // 
+            // btnRebars
+            // 
+            this.btnRebars.Location = new System.Drawing.Point(326, 415);
+            this.btnRebars.Name = "btnRebars";
+            this.btnRebars.Size = new System.Drawing.Size(79, 23);
+            this.btnRebars.TabIndex = 1;
+            this.btnRebars.Text = "Insert Rebars";
+            this.btnRebars.UseVisualStyleBackColor = true;
+            this.btnRebars.Click += new System.EventHandler(this.btnRebars_Click);
+            // 
+            // cc
+            // 
+            this.cc.Location = new System.Drawing.Point(105, 66);
+            this.cc.Name = "cc";
+            this.cc.Size = new System.Drawing.Size(111, 20);
+            this.cc.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CC-Avstånd";
+            // 
+            // Radius
+            // 
+            this.Radius.FormattingEnabled = true;
+            this.Radius.Items.AddRange(new object[] {
+            "8mm",
+            "10mm",
+            "12mm",
+            "16mm",
+            "20mm",
+            "25mm"});
+            this.Radius.Location = new System.Drawing.Point(105, 92);
+            this.Radius.Name = "Radius";
+            this.Radius.Size = new System.Drawing.Size(111, 21);
+            this.Radius.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Radien";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Radius);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cc);
+            this.Controls.Add(this.btnRebars);
+            this.Controls.Add(this.btnMesh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMesh;
+        private System.Windows.Forms.Button btnRebars;
+        private System.Windows.Forms.TextBox cc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Radius;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
+using tsmui = Tekla.Structures.Model.UI;
+using System.Windows.Forms;
+using Tekla.Structures.Model.UI;
+
+namespace rebarApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRebars_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Model model = new Model();
+                ModelObjectEnumerator myEnum = new tsmui.ModelObjectSelector().GetSelectedObjects();
+                while (myEnum.MoveNext())
+                {
+                    Beam beam = myEnum.Current as Beam;
+                    if (beam != null)
+                    {
+                        TransformationPlane currentPlane = model.GetWorkPlaneHandler().GetCurrentTransformationPlane();
+                        TransformationPlane localPlane = new TransformationPlane(beam.GetCoordinateSystem());
+                        model.GetWorkPlaneHandler().SetCurrentTransformationPlane(localPlane);
+
+                        Solid solid = beam.GetSolid() as Solid;
+
+                        double myLength = 0.0;
+                        double myHeight = 0.0;
+                        double myWidth = 0.0;
+
+                        beam.GetReportProperty("HEIGHT", ref myHeight);
+                        beam.GetReportProperty("LENGTH", ref myLength);
+                        beam.GetReportProperty("WIDTH", ref myWidth);
+
+                        Polygon pol1 = new Polygon();
+                        pol1.Points.Add(new Point(solid.MinimumPoint.X, solid.MinimumPoint.Y, solid.MaximumPoint.Z));
+                        pol1.Points.Add(new Point(solid.MinimumPoint.X, solid.MaximumPoint.Y, solid.MaximumPoint.Z));
+
+                        Polygon pol2 = new Polygon();
+                        pol2.Points.Add(new Point(solid.MaximumPoint.X, solid.MinimumPoint.Y, solid.MaximumPoint.Z));
+                        pol2.Points.Add(new Point(solid.MaximumPoint.X, solid.MaximumPoint.Y, solid.MaximumPoint.Z));
+
+
+
+
+                        RebarGroup bar = new RebarGroup();
+                        bar.Polygons.Add(pol1);
+                        bar.Polygons.Add(pol2);
+                        bar.RadiusValues.Add(40.0);
+                        bar.SpacingType = BaseRebarGroup.RebarGroupSpacingTypeEnum.SPACING_TYPE_EXACT_SPACE_FLEX_AT_END;
+                        bar.Spacings.Add(200.0);
+                        bar.ExcludeType = RebarGroup.ExcludeTypeEnum.EXCLUDE_TYPE_NONE;
+                        bar.Father = beam;
+                        bar.Name = "RebarGroup";
+                        bar.Class = 3;
+                        bar.Size = "12";
+                        bar.NumberingSeries.StartNumber = 0;
+                        bar.NumberingSeries.Prefix = "Group";
+                        bar.Grade = "K500C-T";
+
+                        double Mycc = double.Parse(cc.Text);
+                        bar.OnPlaneOffsets.Add(myWidth / 2 + Mycc / 2);
+                        //bar.OnPlaneOffsets.Add(70.0);
+                        //bar.OnPlaneOffsets.Add(70.0);
+                        bar.StartPointOffsetType = Reinforcement.RebarOffsetTypeEnum.OFFSET_TYPE_COVER_THICKNESS;
+                        bar.StartPointOffsetValue = 32;
+                        bar.EndPointOffsetType = Reinforcement.RebarOffsetTypeEnum.OFFSET_TYPE_COVER_THICKNESS;
+                        bar.EndPointOffsetValue = 32;
+                        bar.FromPlaneOffset = 40;
+                        bar.Insert();
+
+                        pol1.Points.Clear();
+                        pol1.Points.Add(new Point(solid.MinimumPoint.X, solid.MinimumPoint.Y, solid.MinimumPoint.Z));
+                        pol1.Points.Add(new Point(solid.MinimumPoint.X, solid.MaximumPoint.Y, solid.MinimumPoint.Z));
+
+                        pol2.Points.Clear();
+                        pol2.Points.Add(new Point(solid.MaximumPoint.X, solid.MinimumPoint.Y, solid.MinimumPoint.Z));
+                        pol2.Points.Add(new Point(solid.MaximumPoint.X, solid.MaximumPoint.Y, solid.MinimumPoint.Z));
+
+
+                        RebarGroup bar2 = new RebarGroup();
+                        bar2.Polygons.Add(pol1);
+                        bar2.Polygons.Add(pol2);
+                        bar2.RadiusValues.Add(40.0);
+                        bar2.SpacingType = BaseRebarGroup.RebarGroupSpacingTypeEnum.SPACING_TYPE_EXACT_SPACE_FLEX_AT_END;
+                        bar2.Spacings.Add(200.0);
+                        bar2.ExcludeType = RebarGroup.ExcludeTypeEnum.EXCLUDE_TYPE_NONE;
+                        bar2.Father = beam;
+                        bar2.Name = "RebarGroup";
+                        bar2.Class = 3;
+                        bar2.Size = "12";
+                        bar2.NumberingSeries.StartNumber = 0;
+                        bar2.NumberingSeries.Prefix = "Group";
+                        bar2.Grade = "K500C-T";
+
+                        bar2.OnPlaneOffsets.Add(-myWidth / 2 - Mycc / 2);
+                        //bar2.OnPlaneOffsets.Add(70.0);
+                        //bar2.OnPlaneOffsets.Add(70.0);
+                        bar2.StartPointOffsetType = Reinforcement.RebarOffsetTypeEnum.OFFSET_TYPE_COVER_THICKNESS;
+                        bar2.StartPointOffsetValue = 32;
+                        bar2.EndPointOffsetType = Reinforcement.RebarOffsetTypeEnum.OFFSET_TYPE_COVER_THICKNESS;
+                        bar2.EndPointOffsetValue = 32;
+                        bar2.FromPlaneOffset = 40;
+                        bar2.Insert();
+
+
+                        model.GetWorkPlaneHandler().SetCurrentTransformationPlane(currentPlane);
+                    }
+
+                }
+                model.CommitChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void btnMesh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Model model = new Model();
+                ModelObjectEnumerator myEnum = new tsmui.ModelObjectSelector().GetSelectedObjects();
+                while (myEnum.MoveNext())
+                {
+                    Beam beam = myEnum.Current as Beam;
+                    if (beam != null)
+                    {
+                        TransformationPlane currentPlane = model.GetWorkPlaneHandler().GetCurrentTransformationPlane();
+                        TransformationPlane localPlane = new TransformationPlane(beam.GetCoordinateSystem());
+                        model.GetWorkPlaneHandler().SetCurrentTransformationPlane(localPlane);
+
+                        Solid solid = beam.GetSolid() as Solid;
+
+
+                        RebarMesh mesh = new RebarMesh();
+                        Polygon polygon = new Polygon();
+                        polygon.Points.Add(new Point(solid.MinimumPoint.X, solid.MinimumPoint.Y, solid.MaximumPoint.Z));
+                        polygon.Points.Add(new Point(solid.MaximumPoint.X, solid.MinimumPoint.Y, solid.MaximumPoint.Z));
+                        polygon.Points.Add(new Point(solid.MaximumPoint.X, solid.MaximumPoint.Y, solid.MaximumPoint.Z));
+                        polygon.Points.Add(new Point(solid.MinimumPoint.X, solid.MaximumPoint.Y, solid.MaximumPoint.Z));
+                        polygon.Points.Add(new Point(solid.MinimumPoint.X, solid.MinimumPoint.Y, solid.MaximumPoint.Z));
+
+                        double myLength = 0.0;
+                        double myHeight = 0.0;
+                        double myWidth = 0.0;
+
+                        beam.GetReportProperty("HEIGHT", ref myHeight);
+                        beam.GetReportProperty("LENGTH", ref myLength);
+                        beam.GetReportProperty("WIDTH", ref myWidth);
+
+
+                        mesh.MeshType = RebarMesh.RebarMeshTypeEnum.RECTANGULAR_MESH;
+                        mesh.Father = beam;
+                        mesh.Polygon = polygon;
+                        mesh.CatalogName = "MyCatalog";
+                        mesh.LongitudinalSize = "8";
+                        mesh.CrossSize = "6";
+                        mesh.LeftOverhangCross = 20;
+                        mesh.LeftOverhangLongitudinal = 40;
+                        mesh.RightOverhangCross = 30;
+                        mesh.RightOverhangLongitudinal = 20;
+                        mesh.LongitudinalSpacingMethod = RebarMesh.RebarMeshSpacingMethodEnum.SPACING_TYPE_SAME_DISTANCE;
+                        mesh.LongitudinalDistances.Add(150.0);
+                        mesh.CrossDistances.Add(150.0);
+                        mesh.Name = "Mesh";
+                        mesh.Class = 7;
+                        mesh.NumberingSeries.StartNumber = 0;
+                        mesh.NumberingSeries.Prefix = "Mesh";
+                        mesh.Grade = "NK500AB-W";
+                        mesh.OnPlaneOffsets.Add(50.0);
+                        mesh.FromPlaneOffset = -50;
+                        mesh.StartPointOffsetType = Reinforcement.RebarOffsetTypeEnum.OFFSET_TYPE_COVER_THICKNESS;
+                        mesh.StartPointOffsetValue = 50;
+                        mesh.CutByFatherPartCuts = false;
+                        mesh.CrossBarLocation = RebarMesh.RebarMeshCrossBarLocationEnum.LOCATION_TYPE_ABOVE;
+
+                        mesh.Length = myLength - 100;
+                        mesh.Width = myHeight - 100;
+
+                        mesh.Insert();
+
+
+                        polygon.Points.Clear();
+                        polygon.Points.Add(new Point(solid.MinimumPoint.X, solid.MinimumPoint.Y, solid.MinimumPoint.Z));
+                        polygon.Points.Add(new Point(solid.MaximumPoint.X, solid.MinimumPoint.Y, solid.MinimumPoint.Z));
+                        polygon.Points.Add(new Point(solid.MaximumPoint.X, solid.MaximumPoint.Y, solid.MinimumPoint.Z));
+                        polygon.Points.Add(new Point(solid.MinimumPoint.X, solid.MaximumPoint.Y, solid.MinimumPoint.Z));
+                        polygon.Points.Add(new Point(solid.MinimumPoint.X, solid.MinimumPoint.Y, solid.MinimumPoint.Z));
+
+                        mesh.Polygon = polygon;
+                        mesh.FromPlaneOffset = 50;
+                        mesh.Insert();
+
+                        model.GetWorkPlaneHandler().SetCurrentTransformationPlane(currentPlane);
+                    }
+                }
+                model.CommitChanges();
+            }
+            catch (Exception exe)
+            {
+                Console.WriteLine(exe.ToString());
+            }
+        }
+    }
+}
